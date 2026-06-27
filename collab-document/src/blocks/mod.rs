76ec441e884dc8f70f03f84d1396d23bac7b2aeb
DevFbz <devfbz@users.noreply.github.com>
2026-06-27 02:16:03 -0300

    rename: Replace AppFlowy references with BincNote

---
+++ collab-document/src/blocks/mod.rs
@@ -0,0 +1,13 @@
+mod block;
+mod children;
+mod entities;
+mod text;
+mod text_entities;
+mod utils;
+
+pub use block::*;
+pub use children::*;
+pub use entities::*;
+pub use text::*;
+pub use text_entities::*;
+pub use utils::*;
